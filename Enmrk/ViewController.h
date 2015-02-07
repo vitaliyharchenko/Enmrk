e0c9f84b191eb34700897d84d97fe537b5d37ba2
Vitaliy Kharchenko <harchenko.grape@gmail.com>
2015-02-07 10:40:37 +0500

    Initial Commit

---
+++ Enmrk/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Enmrk
+//
+//  Created by Vitaliy Harchenko on 07.02.15.
+//  Copyright (c) 2015 Vitaliy Harchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Enmrk/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Enmrk
+//
+//  Created by Vitaliy Harchenko on 07.02.15.
+//  Copyright (c) 2015 Vitaliy Harchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
